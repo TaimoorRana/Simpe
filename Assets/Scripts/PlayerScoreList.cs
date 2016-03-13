9f4521b677cc29e98bc0ecd94a5da7928f4ab479
Taimoor Rana <taimoorrana1@hotmail.com>
2016-03-13 00:04:18 -0500

    Scoreboard Partially Completed

---
+++ Assets/Scripts/PlayerScoreList.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScoreList : MonoBehaviour {
+	public GameObject PlayerScoreUI;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 6; i++) {
+			GameObject score = Instantiate (PlayerScoreUI);
+			score.transform.SetParent (transform);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ScoreManager : MonoBehaviour {
+	Dictionary<string,int> scores;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Init(){
+		if (scores != null)
+			return;
+		
+		scores = new Dictionary<string, int> ();
+	}
+
+	public int GetScore(string username){
+		Init ();
+		if (!scores.ContainsKey (username)) {
+			return 0;
+		}
+		return scores [username];
+	}
+
+	public void SetScore(string username, int score){
+		Init ();
+		scores.Add (username, score);
+	}
+
+	public void ChangeScore(string username, int score){
+		Init ();
+		if (scores.ContainsKey (username)) {
+			scores [username] = score;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
